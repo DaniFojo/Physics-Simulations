b3efadff4aabf778a27b022f8a3d88ab32b01862
Dani Fojo <dani.fojo@gmail.com>
2016-10-07 02:33:47 +0200

    Update pendulum.m

--- matlab/pendulum.m
+++ matlab/pendulum.m
@@ -1,13 +1,13 @@
-%This is a numerical simulation of a pendulum with a massless rod.
+% This is a numerical simulation of a pendulum with a massless rod.
  
 % User Defined Parameters
-%Define external parameters
+% Define external parameters
 G = 9.8;
 deltaTime = 1/500; %Decreasing this will increase simulation accuracy
 endTime = 100000;
 beta = 0.1; %air friction constant
  
-%Define pendulum
+% Define pendulum
 rodPivotPoint = [2 2]; %rectangular coordinates
 rodLength = 1;
 mass = 1; %of the bob
@@ -21,7 +21,7 @@ assert(radius < rodLength,'Pendulum bob radius must be less than the length of t
  
 position = rodPivotPoint - (rodLength*[-sin(theta) cos(theta)]); %in rectangular coordinates
  
-%Generate graphics, render pendulum
+% Generate graphics, render pendulum
 figure;
 axesHandle = gca;
 xlim(axesHandle, [(rodPivotPoint(1) - rodLength - radius) (rodPivotPoint(1) + rodLength + radius)] );
@@ -35,21 +35,21 @@ lineHandle = line([rodPivotPoint(1) position(1)],...
      [rodPivotPoint(2) position(2)]); %pendulum rod
 hold off
  
-%Run simulation, all calculations are performed in cylindrical coordinates
+% Run simulation, all calculations are performed in cylindrical coordinates
 for time = (deltaTime:deltaTime:endTime)
  
     drawnow; %Forces MATLAB to render the pendulum
  
-    %Find total force
+    % Find total force
     gravitationalForceCylindrical = [-mass*G*cos(theta) -mass*G*sin(theta)];
     
     frictionForceCylindrical = -1*beta*velocity;
  
-    %This code is just incase you want to add more forces,e.g friction
+    % This code is just incase you want to add more forces,e.g friction
     totalForce = gravitationalForceCylindrical + frictionForceCylindrical; 
  
-    %If the rod isn't massless or is a spring, etc., modify this line
-    %accordingly
+    % If the rod isn't massless or is a spring, etc., modify this line
+    % accordingly
     rodForce = [-totalForce(1) 0]; %cylindrical coordinates
  
     totalForce = totalForce + rodForce;
@@ -62,7 +62,7 @@ for time = (deltaTime:deltaTime:endTime)
  
     position = rodPivotPoint - (rodLength*[-sin(theta) cos(theta)]);
  
-    %Update figure with new position info
+    % Update figure with new position info
     set(rectHandle,'Position',[(position - radius/2) radius radius]);
     set(lineHandle,'XData',[rodPivotPoint(1) position(1)],'YData',...
         [rodPivotPoint(2) position(2)]);
